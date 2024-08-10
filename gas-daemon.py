fb6252dbfe9ac2d3af99f66ea19a6dbeb487dee5
8to <otooleat@ornl.gov>
2024-08-09 21:02:23 -0400

    Change prints to logging

--- gas-daemon.py
+++ gas-daemon.py
@@ -6,20 +6,27 @@ from watchdog.events import FileSystemEventHandler
 from daemoniker import Daemonizer
 import configparser
 import os
+import logging
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M",
+)
 
 class MyEventHandler(FileSystemEventHandler):
     def __init__(self, directory):
         self.directory = directory
         self.debounce_start = time.time()  # don't call sync more than once a second
         # Go ahead and sync as soon as you start
-        print("running sync on start up")
+        logging.info("Running sync on start up")
         subprocess.run(["git-auto-sync", "sync"], cwd=self.directory, check=True)
-        print("initial sync complete")
+        logging.info("initial sync complete")
 
     def on_modified(self, event):
         if not event.is_directory and not self.is_hidden(event.src_path):  # Ignore directory changes and files in hidden folders
-            print(f'{event.src_path} has been modified. calling callback.')
+            logging.info(f'{event.src_path} has been modified. calling callback.')
             self.run_git_auto_sync()
 
     def run_git_auto_sync(self):
@@ -27,11 +34,11 @@ class MyEventHandler(FileSystemEventHandler):
         if debounce_end - self.debounce_start > 5:
             self.debounce_start = time.time()
             # Change the working directory and run the command
-            print("Running git-auto-sync")
+            logging.info("Running git-auto-sync")
             subprocess.run(["git-auto-sync", "sync"], cwd=self.directory, check=True)
-            print("Completed git-auto-sync")
+            logging.info("Completed git-auto-sync")
         else:
-            print("Not syncing again - synced within last 5 seconds")
+            logging.info("Not syncing again - synced within last 5 seconds")
 
     def is_hidden(self, path):
         """Says if a path contains a hidden folder"""
@@ -39,9 +46,9 @@ class MyEventHandler(FileSystemEventHandler):
         return False
 
 def job(directory):
-    print("Running scheduled git-auto-sync...")
+    logging.info("Running scheduled git-auto-sync...")
     subprocess.run(["git-auto-sync", "sync"], cwd=directory, check=True)
-    print("Scheduled git-auto-sync is complete")
+    logging.info("Scheduled git-auto-sync is complete")
 
 
 def run_daemon(directory, interval):
